6fae92b6fe128492d5f8f1f4146c180ee1a4100a
unknown <galocvalquihermes@gmail.com>
2025-09-02 15:59:36 -0500

    clase practica

---
+++ PRACTICA1.R
@@ -0,0 +1,63 @@
+# Mi primera practica con R
+x <- "Hola"
+y <- "UNTRM"
+# f(x,y)=x + x^2 + 2xy
+# ayuda:F1 Fn + F1
+
+
+paste(x, y)
+
+paste(x,y, sep =" + ")
+
+paste(x, y)
+
+
+# tidyverse cran
+
+library(tidyverse)
+library(tidyverse)
+library(tidyverse)
+
+
+# pipe
+
+#un objeto no empieza con un numero
+a1 <- "estudiante"
+1a<- "cocina
+
+a_1 <- "objeto""
+a.1 <- "a"
+a_1 <- "b"
+
+
+notas <- c(16, 17,19, 15)
+media <- mean(notas)
+media
+
+
+#ctrl + shift + m => %>%
+m <- c(16, 17,19, 15) %>% 
+  mean()
+m
+
+
+-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
